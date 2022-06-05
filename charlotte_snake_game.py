7a5d1377f0f2a50514f9df2392c7e1c8758cbb98
Yuyi Fu <yfu@seismic.com>
2022-06-05 12:27:37 -0400

    learning pygame

---
+++ charlotte_snake_game.py
@@ -0,0 +1,53 @@
+#Snake Tutorial Python
+import os
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+width = 700
+height = 700
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("my game")
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+purple = (150,0,100)
+FPS = 60
+
+green_grapes = pygame.image.load(os.path.join('assets','green_grapes.jpeg'))
+green_grapes = pygame.transform.scale(green_grapes_image, (55, 40))
+banana = pygame.image.load(os.path.join('assets','banana.jpeg'))
+banana = pygame.transform.scale(banana_image, (55, 40))
+monkey_eating_banana = pygame.image.load(os.path.join('assets','monkey_eating_banana.jpeg'))
+monkey_eating_banana = pygame.transform.scale(monkey_eating_banana_image, (55, 40))
+monkey_eating = pygame.image.load(os.path.join('assets','monkey_eating.jpeg'))
+monkey_eating = pygame.transform.scale(monkey_eating_image (55, 40))
+
+def draw_window(colour):
+  win.fill(colour)
+  win.blit(green_grapes,(200,100))
+
+
+  win.blit(banana(400,100))
+  pygame.display.update()
+
+
+def main():
+  clock = pygame.time.Clock()
+  run = True
+  while run:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+  
+    draw_window(white)
+  
+  pygame.quit()
+
+
+if __name__ == "__main__":
+  main()
